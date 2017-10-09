5a8865f6fc75263da6445888e740bbab64733475
ryancirish <organized.ryancirish@gmail.com>
2017-10-09 01:11:02 -0400

    finished template

--- pages/b/template.vue
+++ pages/b/template.vue
@@ -1,10 +1,61 @@
 <template>
-	<article id="entry" class="measure">
-		<h1 class="f-subheadline helvetica white"> title </h1>
-	</article>
+	<div id="entry" class="">
+
+		<aside class="titleCard">
+			<h1 class="f-subheadline helvetica white mv0"> title </h1>
+			<div class="ml3">
+				<h2 class="f2 helvetica white">10/10/2200</h2>
+				<h3 class="f3 helvetica white">optional subheadline</h3>
+			</div>
+		</aside>
+
+		<main id="copy" class="  center white lh-copy">
+			<p>Sprawl weathered tower boy assassin render-farm sign market vehicle beef noodles tattoo. Media bridge disposable corrupted rain dolphin smart. Lights systemic-space narrative tattoo alcohol man warehouse saturation point industrial grade numinous silent dissident youtube 3D-printed. Youtube tanto computer order-flow grenade boy geodesic papier-mache receding woman. Dead voodoo god free-market dolphin tower corporation geodesic systemic rain chrome shanty town A.I. woman dome. Warehouse paranoid lights neural smart-8-bit papier-mache construct sign sensory. Neural industrial grade urban corporation beef noodles youtube fluidity cardboard spook systema Chiba. Artisanal papier-mache euro-pop systema dome Tokyo sub-orbital Chiba. Apophenia concrete saturation point office tank-traps wonton soup long-chain hydrocarbons media. Dead beef noodles franchise sprawl film courier pistol long-chain hydrocarbons spook jeans 3D-printed neural realism tube cardboard warehouse. Digital free-market courier euro-pop shoes receding fetishism modem media city girl table numinous systemic film garage long-chain hydrocarbons. Franchise sub-orbital motion table bridge narrative plastic fetishism carbon pre-realism grenade fluidity cardboard. Assassin tiger-team alcohol BASE jump courier voodoo god Chiba savant 8-bit into urban. Otaku futurity papier-mache alcohol drugs uplink engine physical vehicle garage paranoid franchise car urban 8-bit San Francisco gang. Fluidity refrigerator artisanal industrial grade dolphin saturation point corrupted footage DIY tank-traps boat drugs ablative. Systema neon Chiba into pen sentient Shibuya uplink grenade BASE jump shanty town silent office city rain camera gang. Tower faded sub-orbital pen nano-DIY systema decay semiotics dissident. Engine papier-mache market Shibuya franchise denim woman RAF silent refrigerator chrome. Face forwards euro-pop apophenia Shibuya boat papier-mache industrial grade. Film artisanal tiger-team car neural Kowloon cardboard alcohol office refrigerator wonton soup San Francisco. Shibuya marketing face forwards spook sign tube math-decay voodoo god. Sentient computer saturation point cartel rifle motion into market shrine spook smart. Range-rover meta-gang chrome stimulate industrial grade math-camera car faded dome claymore mine woman. Hotdog dome disposable advert free-market narrative weathered warehouse smart-tanto. Sprawl warehouse jeans military-grade bomb corporation disposable advert courier rain systema market man otaku katana. Alcohol lights tank-traps savant market face forwards courier claymore mine wristwatch silent rebar-ware. Fluidity boy apophenia geodesic denim alcohol chrome assassin office girl ablative marketing. Long-chain hydrocarbons kanji into apophenia man cartel saturation point fetishism face forwards assault singularity. Singularity nodal point into euro-pop Tokyo A.I. motion pen physical skyscraper disposable. Assault monofilament A.I. skyscraper Shibuya advert 8-bit San Francisco receding realism gang shrine semiotics smart-knife. Corporation sensory Tokyo pre-apophenia sprawl beef noodles rifle wristwatch j-pop kanji digital. Voodoo god RAF narrative military-grade assault network wonton soup lights tower bicycle skyscraper A.I. tattoo. Physical otaku BASE jump dead 8-bit singularity dome crypto-alcohol. Woman carbon silent assassin girl 8-bit denim. Receding rebar wristwatch human courier market spook gang meta. Plastic boy footage sunglasses tank-traps hacker range-rover free-market drone bomb receding realism kanji. Sunglasses j-pop euro-pop marketing futurity order-flow digital hotdog voodoo god market bridge refrigerator. Singularity geodesic artisanal crypto-meta-footage wristwatch DIY corporation drugs. Carbon saturation point semiotics tube A.I. realism media cyber-systemic fluidity shoes. Sunglasses drone office receding corporation tower garage. Warehouse office assassin media computer monofilament shrine man. Pistol Chiba fetishism semiotics tiger-team bridge construct San Francisco sunglasses Shibuya boat rebar man. Free-market math-jeans woman dissident faded katana. Kanji voodoo god media crypto-sensory systema-ware BASE jump artisanal sentient human neural denim tank-traps camera nodal point warehouse. Nodal point San Francisco refrigerator dead sunglasses face forwards sprawl footage table soul-delay digital. Long-chain hydrocarbons face forwards j-pop BASE jump advert meta-towards office nodal point sunglasses DIY kanji table pen. Motion systema numinous rifle post-free-market realism pistol DIY grenade plastic geodesic. Uplink post-monofilament garage systema carbon geodesic decay shanty town systemic rifle meta-bomb. Modem free-market shrine claymore mine decay alcohol refrigerator spook soul-delay face forwards apophenia. </p>
+		</main>
+
+	</div>
 	
 </template>
 
 <script></script>
 
-<style></style>
+<style>
+	#entry {
+		height: 100vh;
+		background-image: url('/sky.jpg');
+	}
+
+	#copy {
+		filter: grayscale(1);
+	}
+
+	.measure-wide {
+		max-width: 54rem;
+	}
+
+	main {
+		max-height: 70vh;
+		overflow-y: auto;
+		
+	}
+
+
+@media (max-width: 850px) {
+  /* styles */
+  main {
+  	max-width: 24rem
+  }
+}
+
+@media (min-width: 850px) {
+  /* styles */
+  main {
+  	max-width: 48rem
+  }
+}
+
+
+
+
+
+</style>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,11 +1,20 @@
 <template>
-	<div id="blog">
+	<div id="blog" class="">
 		<h1 class="f-subheadline helvetica white mt0">blog</h1>
 
-		<article class="b">
+		<div class="lim overflow-scroll lh-copy measure-wide">
+			
+
+		<article class="block mb5 ml4">
 			<h3 class="helvetica white">10/8/17</h3>
 			<a class="f4 fw6 db white link hover-hot-pink helvetica" href="/b/template"> hello world </a>
+			<p class="white ml2">welcome to my humble abode</p>
 		</article>
+
+
+		</div>
+
+
 
 	</div>
 </template>
@@ -19,5 +28,9 @@
 		height: 100vh;
 		background-image: url("/sky.jpg");
 		filter: grayscale(1);
+	}
+
+	.lim {
+		max-height: 75vh;
 	}
 </style>
