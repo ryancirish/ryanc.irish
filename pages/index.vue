6c75bd5008f99b88fc04f5c39734dcd2929e11f4
ryancirish <organized.ryancirish@gmail.com>
2017-10-11 03:02:26 -0400

    fourth

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <h1 class="f-headline lh-solid z helvetica white fw9">RYAN C. IRISH</h1>
     
     <div class="cf">
-      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="/about"> about </a></h1>
-      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="/blog"> blog </a></h1>
-      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="mailto:me@ryanc.irish"> contact </a></h1>
-      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="https://github.com/ryancirish"> github </a></h1>
+      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="/about" class="white"> about </a></h1>
+      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="/blog" class="white"> blog </a></h1>
+      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="mailto:me@ryanc.irish" class="white"> contact </a></h1>
+      <h1 class="helvetica f-subheadline white hover-bg-navy"><a href="https://github.com/ryancirish" class="white"> github </a></h1>
     </div>
       
   </div>
