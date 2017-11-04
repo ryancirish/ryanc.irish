22f38d48b8011f16d7e561d06c35ceb37432595e
ryancirish <organized.ryancirish@gmail.com>
2017-11-04 18:15:14 -0400

    minor update

---
+++ pages/b/new.vue
@@ -0,0 +1,71 @@
+<template>
+	<div id="entry" class="">
+
+		<aside class="titleCard">
+			<h1 class="f-subheadline helvetica white mv0"> misc. </h1>
+			<div class="ml3">
+				<h2 class="f2 helvetica white">11/4/2017</h2>
+				<h3 class="f3 helvetica white"></h3>
+			</div>
+		</aside>
+
+		<main id="copy" class="f4 center white lh-copy">
+
+			<p>Hello again, long time no see. Added a couple repos to <a href="http://www.github.com/ryancirish">my github</a> that are going to serve as repositories for generic code that been useful in my recent client projects. Also, one of my main items in my research stack this week is distributing this thru zeit or reconfiguring my caddy dist to pull straight from the hub. That should be a better vehicle for continuous integration for a small static distro such as this.</p>
+
+			<p style="margin-bottom:0px;">-rci</p>
+		</main>
+
+	</div>
+	
+</template>
+
+<script></script>
+
+<style>
+
+	#entry {
+		min-height: 100vh;
+		background-image: url('/sky.jpg');
+		background-repeat: repeat-y;
+		background-size: 
+
+	}
+
+	#copy {
+		filter: grayscale(1);
+	}
+
+	.measure-wide {
+		max-width: 54rem;
+	}
+
+	main {
+	
+		
+	}
+
+
+@media (max-width: 850px) {
+  /* styles */
+  main {
+  	max-width: 24rem
+  }
+
+  #entry {
+  
+  }
+}
+
+@media (min-width: 850px) {
+  /* styles */
+  main {
+  	max-width: 48rem
+  }
+}
+
+
+
+
+
+</style>
--- pages/b/template.vue
+++ pages/b/template.vue
@@ -2,15 +2,21 @@
 	<div id="entry" class="">
 
 		<aside class="titleCard">
-			<h1 class="f-subheadline helvetica white mv0"> title </h1>
+			<h1 class="f-subheadline helvetica white mv0"> hello world </h1>
 			<div class="ml3">
-				<h2 class="f2 helvetica white">10/10/2200</h2>
-				<h3 class="f3 helvetica white">optional subheadline</h3>
+				<h2 class="f2 helvetica white">10/9/2017</h2>
+				<h3 class="f3 helvetica white"></h3>
 			</div>
 		</aside>
 
-		<main id="copy" class="  center white lh-copy">
-			<p>Sprawl weathered tower boy assassin render-farm sign market vehicle beef noodles tattoo. Media bridge disposable corrupted rain dolphin smart. Lights systemic-space narrative tattoo alcohol man warehouse saturation point industrial grade numinous silent dissident youtube 3D-printed. Youtube tanto computer order-flow grenade boy geodesic papier-mache receding woman. Dead voodoo god free-market dolphin tower corporation geodesic systemic rain chrome shanty town A.I. woman dome. Warehouse paranoid lights neural smart-8-bit papier-mache construct sign sensory. Neural industrial grade urban corporation beef noodles youtube fluidity cardboard spook systema Chiba. Artisanal papier-mache euro-pop systema dome Tokyo sub-orbital Chiba. Apophenia concrete saturation point office tank-traps wonton soup long-chain hydrocarbons media. Dead beef noodles franchise sprawl film courier pistol long-chain hydrocarbons spook jeans 3D-printed neural realism tube cardboard warehouse. Digital free-market courier euro-pop shoes receding fetishism modem media city girl table numinous systemic film garage long-chain hydrocarbons. Franchise sub-orbital motion table bridge narrative plastic fetishism carbon pre-realism grenade fluidity cardboard. Assassin tiger-team alcohol BASE jump courier voodoo god Chiba savant 8-bit into urban. Otaku futurity papier-mache alcohol drugs uplink engine physical vehicle garage paranoid franchise car urban 8-bit San Francisco gang. Fluidity refrigerator artisanal industrial grade dolphin saturation point corrupted footage DIY tank-traps boat drugs ablative. Systema neon Chiba into pen sentient Shibuya uplink grenade BASE jump shanty town silent office city rain camera gang. Tower faded sub-orbital pen nano-DIY systema decay semiotics dissident. Engine papier-mache market Shibuya franchise denim woman RAF silent refrigerator chrome. Face forwards euro-pop apophenia Shibuya boat papier-mache industrial grade. Film artisanal tiger-team car neural Kowloon cardboard alcohol office refrigerator wonton soup San Francisco. Shibuya marketing face forwards spook sign tube math-decay voodoo god. Sentient computer saturation point cartel rifle motion into market shrine spook smart. Range-rover meta-gang chrome stimulate industrial grade math-camera car faded dome claymore mine woman. Hotdog dome disposable advert free-market narrative weathered warehouse smart-tanto. Sprawl warehouse jeans military-grade bomb corporation disposable advert courier rain systema market man otaku katana. Alcohol lights tank-traps savant market face forwards courier claymore mine wristwatch silent rebar-ware. Fluidity boy apophenia geodesic denim alcohol chrome assassin office girl ablative marketing. Long-chain hydrocarbons kanji into apophenia man cartel saturation point fetishism face forwards assault singularity. Singularity nodal point into euro-pop Tokyo A.I. motion pen physical skyscraper disposable. Assault monofilament A.I. skyscraper Shibuya advert 8-bit San Francisco receding realism gang shrine semiotics smart-knife. Corporation sensory Tokyo pre-apophenia sprawl beef noodles rifle wristwatch j-pop kanji digital. Voodoo god RAF narrative military-grade assault network wonton soup lights tower bicycle skyscraper A.I. tattoo. Physical otaku BASE jump dead 8-bit singularity dome crypto-alcohol. Woman carbon silent assassin girl 8-bit denim. Receding rebar wristwatch human courier market spook gang meta. Plastic boy footage sunglasses tank-traps hacker range-rover free-market drone bomb receding realism kanji. Sunglasses j-pop euro-pop marketing futurity order-flow digital hotdog voodoo god market bridge refrigerator. Singularity geodesic artisanal crypto-meta-footage wristwatch DIY corporation drugs. Carbon saturation point semiotics tube A.I. realism media cyber-systemic fluidity shoes. Sunglasses drone office receding corporation tower garage. Warehouse office assassin media computer monofilament shrine man. Pistol Chiba fetishism semiotics tiger-team bridge construct San Francisco sunglasses Shibuya boat rebar man. Free-market math-jeans woman dissident faded katana. Kanji voodoo god media crypto-sensory systema-ware BASE jump artisanal sentient human neural denim tank-traps camera nodal point warehouse. Nodal point San Francisco refrigerator dead sunglasses face forwards sprawl footage table soul-delay digital. Long-chain hydrocarbons face forwards j-pop BASE jump advert meta-towards office nodal point sunglasses DIY kanji table pen. Motion systema numinous rifle post-free-market realism pistol DIY grenade plastic geodesic. Uplink post-monofilament garage systema carbon geodesic decay shanty town systemic rifle meta-bomb. Modem free-market shrine claymore mine decay alcohol refrigerator spook soul-delay face forwards apophenia. </p>
+		<main id="copy" class="f4 center white lh-copy">
+			<p> New domain, same name. I have been sitting on this for a while after the new TLD announcements, so its about time I spooled up the latest stack/tools I've been playing with. Last blog install on the old domain was ghost, and while it is completely sufficent, I have grown to fancy more lightweight tooling. This site is built with Nuxt, which is an abstraction of Vue that has the ability to statically generate assets. At the same time, I have complete control of the installed modules and file structure. Not bad. </p>
+
+			<p>Vue, in general has been amazing to work with, and I am blessed to be "coming up" at a time where such tooling exists. I grew up on the front end, first kindling my love for dev back in elementary school making HTML sites, so the JS world was relatively easy to pick up. I will focus a lot of articles on my adventures and growth using Vue, and whatever else I use along the way. At the moment, I am quite fond of Feathers.js as a backend, and I have been using it to tinker with an experiment/potential MVP. Feathers is database and front end agnostic, and has support for many authentication protocols out of the box. Combine this with axios and Vue integrates perfectly as the view layer. 3 EZ.</p>
+
+			<p>I'll be back soon with whatever comes to mind. There will be no specific format at the moment. For the wall.</p>
+
+			<p style="margin-bottom:0px;">-rci</p>
 		</main>
 
 	</div>
@@ -20,9 +26,13 @@
 <script></script>
 
 <style>
+
 	#entry {
-		height: 100vh;
+		min-height: 100vh;
 		background-image: url('/sky.jpg');
+		background-repeat: repeat-y;
+		background-size: 
+
 	}
 
 	#copy {
@@ -34,8 +44,7 @@
 	}
 
 	main {
-		max-height: 70vh;
-		overflow-y: auto;
+	
 		
 	}
 
@@ -44,6 +53,10 @@
   /* styles */
   main {
   	max-width: 24rem
+  }
+
+  #entry {
+  
   }
 }
 
